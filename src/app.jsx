92db35ba674acdc930a91fd50562e6f9549a5fa4
JM <dlwlals0406@naver.com>
2021-01-26 15:15:43 +0900

    First commit

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.css';
+import Habit from './component/habit';
+
+function App() {
+  const name = 'jimin';
+  const age = undefined;
+  return <Habit/>;
+}
+
+export default App;
